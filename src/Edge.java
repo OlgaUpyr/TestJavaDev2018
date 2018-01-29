d11e516ef0a65d0d0ec486db37a160c242852f84
OlgaUpyr <olga.upyr1@gmail.com>
2018-01-29 18:59:03 +0200

    implement algorithm of searching the best product name

--- src/Edge.java
+++ src/Edge.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Edge {
     private String name;
     private int weight;
+    private boolean visited;
+    private List<Edge> neighbours;
+
+
     public Edge(){}
 
     public Edge(String name, int weight){
         this.name = name;
         this.weight = weight;
+        this.visited = false;
+        this.neighbours=new ArrayList<>();
+    }
+
+    public void addNeighbours(Edge neighbourEdge) {
+        this.neighbours.add(neighbourEdge);
     }
 
     public String getName() {
@@ -27,5 +40,17 @@ public class Edge {
     @Override
     public String toString() {
         return getName() + ", " + getWeight();
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public List<Edge> getNeighbours() {
+        return neighbours;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,54 +6,199 @@ public class Main {
     private static final String OUTPUT_FILENAME = "C:\\Users\\Oly\\IdeaProjects\\" +
             "TestJavaDev2018\\src\\IOFiles\\bestProductNameAnswer.json";
 
-    private static List<Product> productList;
-    private static Map<String, List<Edge>> graph;
+    private static List<Edge> graph;
+    private static List<List<Edge>> availableNames;
+    private static String bestName = "";
+    private static int edgeIndex = 0;
+    private static int indexDFS = 0;
+    private static boolean indicator = false;
+    private static boolean checkNewName = false;
 
-    public static boolean isEdgeExist(String currentKeyName, String currentValueName){
-        for (int l = 0; l < graph.get(currentKeyName).size(); l++) {
-            Edge currEdge = graph.get(currentKeyName).get(l);
-            if (currentValueName.equals(currEdge.getName())) {
-                currEdge.setWeight(currEdge.getWeight() + 1);
-                return true;
+
+    private static int getNeighbourEdgeIndex(String currentValueName, int index){
+        for (int l = 0; l < graph.size(); l++) {
+            Edge currEdge = graph.get(l);
+            for (int i = 0; i < currEdge.getNeighbours().size(); i++) {
+                if (currentValueName.toLowerCase().equals(currEdge.getNeighbours().get(i).getName().toLowerCase()) && l == index) {
+                    return i;
+                }
             }
+        }
+        return -1;
+    }
+
+    private static int getNeighbourEdgeWeight(String currentValueName){
+        for (int l = 0; l < graph.size(); l++) {
+            Edge currEdge = graph.get(l);
+            for (int i = 0; i < currEdge.getNeighbours().size(); i++) {
+                if (currentValueName.toLowerCase().equals(currEdge.getNeighbours().get(i).getName().toLowerCase())) {
+                    return currEdge.getNeighbours().get(i).getWeight() + 1;
+                }
+            }
+        }
+        return 1;
+    }
+
+    private static int getKeyEdgeIndex(String currentKeyName){
+        for (int i = 0; i < graph.size(); i++) {
+            if(graph.get(i).getName().toLowerCase().equals(currentKeyName.toLowerCase()))
+                return i;
+        }
+        return -1;
+    }
+
+    private static boolean isEdgeNameExist(String name, int index){
+        for (int i = 0; i < availableNames.get(index).size(); i++) {
+            if(name.toLowerCase().equals(availableNames.get(index).get(i).getName().toLowerCase()))
+                return true;
         }
         return false;
     }
 
+    private static void DFS(Edge edge){
+        if(!isEdgeNameExist(edge.getName(), indexDFS))
+            availableNames.get(indexDFS).add(edge);
+        else {
+            availableNames.add(new ArrayList<>());
+            indexDFS++;
+            checkNewName = true;
+            return;
+        }
+        indicator = false;
+        for (int i = 0; i < graph.size(); i++) {
+            if(graph.get(i).getName().toLowerCase().equals(edge.getName().toLowerCase())) {
+                edgeIndex = i;
+                checkNewName = false;
+                indicator = true;
+                break;
+            }
+        }
+        if(!indicator) {
+            availableNames.add(new ArrayList<>());
+            indexDFS++;
+            checkNewName = true;
+            return;
+        }
+        List<Edge> neighbours = graph.get(edgeIndex).getNeighbours();
+        graph.get(edgeIndex).setVisited(true);
+        for (int i = 0; i < neighbours.size(); i++) {
+            Edge e = neighbours.get(i);
+            if(e != null && !e.isVisited()) {
+                if(checkNewName)
+                    availableNames.get(indexDFS).addAll(availableNames.get(indexDFS - 1).subList(0, getEdgeIndex(edge.getName()) + 1));
+                DFS(e);
+            }
+        }
+    }
+
+    private static int getEdgeIndex(String name){
+        for (int i = 0; i < availableNames.get(indexDFS - 1).size(); i++) {
+            if(name.toLowerCase().equals(availableNames.get(indexDFS - 1).get(i).getName().toLowerCase()))
+                return i;
+        }
+        return 0;
+    }
+
+    private static Edge getStartEdge(String name, Integer weight){
+        for (int i = 0; i < graph.size(); i++) {
+            if(graph.get(i).getName().toLowerCase().equals(name.toLowerCase())) {
+                graph.get(i).setWeight(weight);
+                return graph.get(i);
+            }
+        }
+        return new Edge();
+    }
+
     public static void main(String[] args) {
+        List<Product> productList;
+
         JSONReader jsonReader = new JSONReader();
         productList = jsonReader.read(INPUT_FILENAME);
 
         String[] currentName;
-        List<String> startEdges;
-        graph = new LinkedHashMap<>();
+        Map<String, Integer> startEdges;
+        graph = new ArrayList<>();
+        availableNames = new ArrayList<>();
+        availableNames.add(new ArrayList<>());
 
-        for (int i = 0; i < 1; i++) {
-            startEdges = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            indicator = false;
+            startEdges = new HashMap<>();
             graph.clear();
             for (int j = 0; j < productList.get(i).getNames().size(); j++) {
-                currentName = productList.get(i).getNames().get(j).split("\\s");
-                if(!(startEdges.contains(currentName[0])))
-                    startEdges.add(currentName[0]);
+                String pattern = " - ";
+                String currentNameStr = productList.get(i).getNames().get(j);
+                currentNameStr = currentNameStr.replaceAll(pattern, " ");
+                currentName = currentNameStr.split("\\s");
+                for (Map.Entry<String, Integer> edgeEntry: startEdges.entrySet()) {
+                    if(edgeEntry.getKey().toLowerCase().equals(currentName[0].toLowerCase())){
+                        currentName[0] = edgeEntry.getKey();
+                        Integer weight = startEdges.get(currentName[0]);
+                        startEdges.replace(currentName[0], weight + 1);
+                        indicator = true;
+                    }
+                }
+                if(!indicator) {
+                    startEdges.put(currentName[0], 1);
+                }
                 for (int k = 1; k < currentName.length; k++) {
-                    if(!(currentName[k].equals("-") || currentName[k].equals("/"))) {
-                        if (!graph.containsKey(currentName[k - 1]))
-                            graph.put(currentName[k - 1], new LinkedList<>());
-                        if(!isEdgeExist(currentName[k - 1], currentName[k]))
-                            graph.get(currentName[k - 1]).add(new Edge(currentName[k], 1));
+                    int currIndex = getKeyEdgeIndex(currentName[k - 1]);
+                    if (currIndex == -1) {
+                        graph.add(new Edge(currentName[k - 1], 1));
+                        currIndex = graph.size() - 1;
+                    }
+                    int currNeighbourIndex = getNeighbourEdgeIndex(currentName[k], currIndex);
+                    if (currNeighbourIndex == -1) {
+                        graph.get(currIndex).addNeighbours(new Edge(currentName[k], getNeighbourEdgeWeight(currentName[k])));
+                    } else {
+                        Edge edge = graph.get(currIndex).getNeighbours().get(currNeighbourIndex);
+                        edge.setWeight(edge.getWeight() + 1);
                     }
                 }
             }
+
+            for (Map.Entry<String, Integer> edgeEntry: startEdges.entrySet()) {
+                availableNames = new ArrayList<>();
+                availableNames.add(new ArrayList<>());
+                indexDFS = 0;
+                DFS(getStartEdge(edgeEntry.getKey(), edgeEntry.getValue()));
+
+                int wordCount = 0, totalCount = 0, currentWordCount;
+                bestName = "";
+                for (int k = 0; k < availableNames.get(0).size(); k++) {
+                    wordCount += availableNames.get(0).get(k).getWeight();
+                }
+                if(totalCount < wordCount && availableNames.size() == 2
+                        ){
+                    bestName = "";
+                    for (int k = 0; k < availableNames.get(0).size(); k++) {
+                        bestName += availableNames.get(0).get(k).getName() + " ";
+                    }
+                }
+                for (int j = 1; j < availableNames.size() - 1; j++) {
+                    currentWordCount = 0;
+                    for (int k = 0; k < availableNames.get(j).size(); k++) {
+                        currentWordCount += availableNames.get(j).get(k).getWeight();
+                    }
+                    if((wordCount < currentWordCount && totalCount < wordCount) || (wordCount < currentWordCount)) {
+                        totalCount = wordCount;
+                        bestName = "";
+                        for (int k = 0; k < availableNames.get(j).size(); k++) {
+                            bestName += availableNames.get(j).get(k).getName() + " ";
+                        }
+                    }
+                }
+
+                for (int j = 1; j < availableNames.size() - 1; j++) {
+                    for (int k = 0; k < availableNames.get(j).size(); k++) {
+                        System.out.print(availableNames.get(j).get(k) + " ");
+                    }
+                    System.out.println();
+                }
+            }
+            System.out.println(bestName);
+            productList.get(i).setBestName(bestName);
         }
-
-
-//        for (Map.Entry<String, List<Edge>> entry : graph.entrySet()) {
-//            System.out.println("Key = " + entry.getKey());
-//            List<Edge> edgeList = entry.getValue();
-//            for (int i = 0; i< edgeList.size(); i++) {
-//                System.out.println("Value = " + edgeList.get(i).toString());
-//            }
-//        }
 
         JSONWriter jsonWriter = new JSONWriter();
         jsonWriter.write(OUTPUT_FILENAME, productList);
